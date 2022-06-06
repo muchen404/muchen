2791faacfa3b2b5366faaa35ac7e996285a08083
liuyunxiao <75168572@qq.com>
2022-06-07 00:45:44 +0800

    ✨新增(global-config): 添加changelog工具
    添加 conconventional-changelog 用于生成changelog

--- .cz-config.js
+++ .cz-config.js
@@ -33,8 +33,9 @@ module.exports = {
     scope: '选择一个scope (可选):',
     // used if allowCustomScopes is true
     // customScope: 'Denote the SCOPE of this change:',
-    subject: '短说明:\n',
-    body: '长说明，使用"|"换行(可选)：\n',
+    customScope: '请输入文件修改范围(可选)',
+    subject: '请简要描述提交(必填)\n',
+    body: '请输入详细描述，使用"|"换行(可选)：\n',
     breaking: '非兼容性说明 (可选):\n',
     footer: '关联关闭的issue，例如：#31, #34(可选):\n',
     confirmCommit: '确定提交说明?(yes/no)'
