d1d3ecd4a93de325d18ee403281d18b579d44bc4
liuyunxiao <75168572@qq.com>
2022-06-06 23:09:49 +0800

    setup: configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  // 配置包拓展，可以拓展他人集成的规则集
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
